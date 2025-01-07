1c466e88809cddefc82033bd74562c206eddaa56
HSSJW <okqkfkfk@naver.com>
2025-01-07 16:31:28 +0900

    Initial commit

---
+++ src/main/java/com/example/GrowIT/GrowItApplication.java
@@ -0,0 +1,13 @@
+package com.example.GrowIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GrowIT/GrowItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GrowIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
